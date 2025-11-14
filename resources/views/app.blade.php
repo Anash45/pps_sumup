ca6c49b7b9915e971cc5d10517f1f377c66c2593
Syed Anas Bukhari <anas14529@gmail.com>
2025-11-14 06:18:11 +0500

    Fixed favicon.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,6 +7,12 @@
 
     <title inertia>{{ config('app.name', 'Laravel') }}</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="/favicons/android-chrome-192x192.png">
+    <link rel="icon" type="image/png" sizes="512x512" href="/favicons/android-chrome-512x512.png">
+    <link rel="shortcut icon" href="/favicons/favicon.ico">
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
